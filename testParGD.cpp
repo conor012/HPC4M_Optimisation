76f7d40a590cb3b7aad542e5b0ce27611b0bdfd2
conor012 <s1991749@ed.ac.uk>
2021-04-05 17:37:19 +0100

    Update testParGD.cpp

--- testParGD.cpp
+++ testParGD.cpp
@@ -13,16 +13,12 @@ int main(){
   settings.par_momentum = 0.1;
   settings.num_particles = 1000;
 
-  //std::cout << settings <<  std::endl;
-  // Initialise vectors to hold the intial values (which need to be inputted)
   const int d = {2};
   Eggholder f(d);
   
   // Use the gradient descent algorithm to calculate the minimum.
   GradientDescent gd;
   Eigen::VectorXd res = gd.parallel_minimise(f, settings);
-  
-  //std::cout << res  << std::endl;
   
   return 0;
 }
